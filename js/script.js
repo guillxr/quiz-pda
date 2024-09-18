aac29ae15b2b429632349f825734e34714e30334
guillxr <136463238+guillxr@users.noreply.github.com>
2024-09-17 22:47:29 -0300

    update: add show and hide functions

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// remove a class hidden que vem por padrão no elemento passado no parametro
+function show(e) {
+    e.classList.remove('hidden')
+}
+
+// adiciona a class hidden no elemento passado no parametro
+function hide(e) {
+    e.classList.add('hidden')
+}
