ef645c9a9c519d1f29103da2327b1a4b10f6def0
guillxr <136463238+guillxr@users.noreply.github.com>
2024-09-17 22:51:14 -0300

    create: user data and roles

---
+++ js/user.js
@@ -0,0 +1,19 @@
+let user = {
+    username: null,
+    avatar: null,
+    rank: null,
+    xp: null
+}
+
+// puxa os dados do localstorage e guarda no objeto user
+function getUserData() {
+    const userJSON = localStorage.getItem("user")
+    if(userJSON) {
+        const userData = JSON.parse(userJSON)
+        user.username = userData.username
+        user.avatar = userData.avatar
+        user.rank = userData.rank
+        user.xp = userData.xp
+    }
+}
+
