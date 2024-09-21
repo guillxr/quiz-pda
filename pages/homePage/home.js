5b8a1361fc89249ca573965ab59816712748bb1e
guillxr <136463238+guillxr@users.noreply.github.com>
2024-09-20 22:52:42 -0300

    update: create functions for home page

---
+++ pages/homePage/home.js
@@ -0,0 +1,22 @@
+const homeSection = document.getElementById('home')
+const btnHomeNextText = document.getElementById('home-button-text')
+
+// insere uma mensagem e texto no botão caso seja o primeiro acesso
+function insertFirstAcessElements() {
+    insertButtonHomeText('Começar')
+    insertHomeMessageText()
+}
+
+// insere um texto no botão
+function insertButtonHomeText(text) {
+    btnHomeNextText.textContent = text
+}
+
+// insere a mensagem caso seja chamado
+function insertHomeMessageText() {
+    const homeMessage = document.getElementById('home-message')
+    homeMessage.textContent = `Olá, ${user.username}. Você está pronto para começar?`
+}
+
+
+
