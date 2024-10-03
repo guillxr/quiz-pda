676e408ff1e25af69e7e5cf6be2307ce82ee8765
guillxr <136463238+guillxr@users.noreply.github.com>
2024-10-02 21:49:00 -0300

    update: call functions and comments

--- pages/homePage/home.js
+++ pages/homePage/home.js
@@ -1,6 +1,7 @@
 const homeSection = document.getElementById('home')
 const btnHomeNextText = document.getElementById('home-button-text')
 
+
 // insere uma mensagem e texto no botão caso seja o primeiro acesso
 function insertFirstAcessElements() {
     insertButtonHomeText('Começar')
@@ -18,8 +19,10 @@ function insertHomeMessageText() {
     homeMessage.textContent = `Olá, ${user.username}. Seja bem-vindo! Você está pronto para começar?`
 }
 
+// função que chama a pagina de quests
 function nextButtonHome() {
     showQuests()
     hide(homeSection)
     show(quiz)
+    checkSwitchsStates()
 }
