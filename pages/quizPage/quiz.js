0fedf13cca0a48545f5594f23bfd56f0f1575c16
guillxr <136463238+guillxr@users.noreply.github.com>
2024-10-02 21:52:08 -0300

    update: creates sound function and updates functions

--- pages/quizPage/quiz.js
+++ pages/quizPage/quiz.js
@@ -7,8 +7,7 @@ const quizScoreButton = document.getElementById('quiz-score-button')
 const quizScoreText = document.getElementById('quiz-score-text')
 const quizCard = document.getElementById('quiz-card')
 const quizScore = document.getElementById('quiz-score')
-const selectionQuizSection = document.getElementById('selection-quiz-section')
-const selectionQuiz = document.getElementById('selection-quiz')
+const selectionQuest = document.getElementById('selection-quest')
 const currentQuestionElement = document.getElementById('current-question')
 const currentScoreElement = document.getElementById('current-score')
 const currentTimerElement = document.getElementById('current-time')
@@ -21,6 +20,7 @@ let score = 0
 let countdown;
 let seconds = 300
 
+// retorna um novo array randomizado
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -30,9 +30,11 @@ function shuffle(array) {
     return array
 }
 
+// função que inicia o quiz 
 function startQuiz() {
     hideScore()
-    createButton('quiz-next-button', 'Proximo', quizButton, handleNextButton)
+    stopMusic()
+    createButton('quiz-next-button', 'Próximo', quizButton, handleNextButton)
 
 
     const nextButton = document.getElementById('quiz-next-button')
@@ -44,6 +46,7 @@ function startQuiz() {
     showQuestions()
 }
 
+// mostra as questions de acordo com o level e quest capturado pelo botão de seleção de quest/quiz
 function showQuestions() {
     resetState()
     showInfoQuest()
@@ -72,6 +75,7 @@ function showQuestions() {
     }
 }
 
+// exibe as informações atuais sobre a quest/quiz
 function showInfoQuest() {
     currentQuestionElement.innerHTML = `${currentQuestionIndex + 1} / 5`
     currentScoreElement.innerHTML = `${score} XP`
@@ -80,6 +84,7 @@ function showInfoQuest() {
     startCountdown()
 }
 
+// reseta/remove os botões da quest/quiz
 function resetState() {
     if(quizButton.firstChild) {
         if(quizButton.firstChild.id === 'quiz-next-button') {
@@ -94,6 +99,7 @@ function resetState() {
     }
 }
 
+// função que verifica a seleção da resposta
 function selectAnswer(e) {
     const selectedBtn = e.target
     const currentButton = parseInt(selectedBtn.getAttribute('btn-id'))
@@ -103,10 +109,12 @@ function selectAnswer(e) {
     if(isCorrect) {
         selectedBtn.classList.add('correct')
         selectedBtn.classList.remove(`btn-${currentButton}`)
+        playSounds('correct')
         score += 10
     } else {
         selectedBtn.classList.remove(`btn-${currentButton}`)
         selectedBtn.classList.add('incorrect')
+        playSounds('incorrect')
     }
 
     for (const button of answerButtons.children) {
@@ -122,9 +130,10 @@ function selectAnswer(e) {
         button.disabled = true
         correctButton++
     }
-    quizButton.firstChild.style.display = 'block'
+    quizButton.firstChild.style.display = 'flex'
 }
 
+// exibe o score da quest/quiz
 function showScore() {
     resetState()
     stopCountdown()
@@ -141,8 +150,10 @@ function showScore() {
     if(score < 50) {
     createButton('quiz-try-again-button', 'Tentar novamente', quizScoreButton, startQuiz)
     }
+    playSounds('finish')
 }
 
+// reseta/remove o score da quest/quiz
 function hideScore() {
     const finishButton = document.getElementById('quiz-finish-button')
     const tryAgainButton = document.getElementById('quiz-try-again-button')
@@ -158,6 +169,7 @@ function hideScore() {
     quizScoreText.innerHTML = ''
 }
 
+// reseta/remove as informações atuais sobre a quest/quiz
 function resetElements() {
     quizCard.style.display = 'none'
     quizButton.removeChild(quizButton.firstChild);
@@ -167,6 +179,7 @@ function resetElements() {
     currentTimerElement.innerHTML = ''
 }
 
+// adiciona o xp ao finalizar a quest/quiz e atualiza o local storage
 function addScoreInXp() {
     let userTemp = JSON.parse(localStorage.getItem('user'));
     let quizDataTemp = JSON.parse(localStorage.getItem('quiz-data'))
@@ -179,11 +192,12 @@ function addScoreInXp() {
         quizDataTemp[questId].score = score
     }
 
-    localStorage.setItem('user', JSON.stringify(userTemp));
-    localStorage.setItem('quiz-data', JSON.stringify(quizDataTemp));
+    localStorage.setItem('user', JSON.stringify(userTemp))
+    localStorage.setItem('quiz-data', JSON.stringify(quizDataTemp))
     getQuizDataAndUpdateLocal()
 }
 
+// função do botão de next/proximo dentro da quest/quiz
 function handleNextButton() {
     currentQuestionIndex++
     if(currentQuestionIndex < quizData[currentLevelQuestions].questions[currentQuizQuest].quests.length) {
@@ -193,6 +207,7 @@ function handleNextButton() {
     }
 }
 
+// função do botão de continuar/finalizar a quest/quiz que chama as funções dependentes
 function handleQuestEndButton() {
     addScoreInXp()
     stopCountdown()
@@ -200,13 +215,15 @@ function handleQuestEndButton() {
     updateQuizData(currentLevelQuestions, currentQuizQuest)
     showQuests()
     hideScore()
-    show(selectionQuiz)
+    startMusic()
+    show(selectionQuest)
     hide(quizSection)
     questionElement.innerHTML = ''
     score = 0
     resetState()
 }
 
+// inicia o timer/cronometro
 function startCountdown() {
     if (!countdown) {
         countdown = setInterval(() => {
@@ -220,28 +237,45 @@ function startCountdown() {
     }
 }
 
+// para o timer/cronometro
 function stopCountdown() {
     clearInterval(countdown)
     countdown = null
     seconds = 300
 }
 
+// retorna o valor do timer/cronometro formatado
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60)
     const remainingSeconds = seconds % 60
     return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`
 }
 
+// função para criação de botões, contendo seus atributos e funções passados no parametro
 function createButton( id, text, element, func) {
     const button =  document.createElement('button')
+    const nextSvg =`
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+        </svg>
+    `
+    const repeatSvg = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="32" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+            <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
+            <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
+        </svg>
+    `
+
     button.classList.add('button')
     button.id = id
     button.textContent = text
+    button.innerHTML += id === 'quiz-try-again-button' ? repeatSvg : nextSvg
     element.appendChild(button)
 
     button.addEventListener('click', func)
 }
 
+// exibe as estrelas dentro do score de acordo com a pontuação da quest/quiz
 function showStars(currentScore) {
     const stars = document.getElementById('quiz-stars')
     let index = 0
@@ -249,9 +283,13 @@ function showStars(currentScore) {
     for (const star of stars.children) {
         const starValue = parseInt(star.getAttribute('data-value'))
 
+        if(star.classList.contains('completed')) {
+            star.classList.remove('completed')
+        }
+
         if (starValue <= currentScore) {
             setTimeout(() => {
-                star.classList.add('completed');
+                star.classList.add('completed')
             }, index * 300)
         } else {
             star.classList.remove('completed')
@@ -259,3 +297,32 @@ function showStars(currentScore) {
         index++
     }
 }
+
+// função que inicia um som de acordo com o parametro passado
+function playSounds(sound) {
+    const audioCorrectAnswer = document.getElementById('audio-correct-answer')
+    const audioIncorrectAnswer = document.getElementById('audio-incorrect-answer')
+    const audioFinishQuiz = document.getElementById('audio-finish-quiz')
+    const { switchStateSound } = JSON.parse(localStorage.getItem('switchsState'))
+
+    if(switchStateSound === true) {
+        switch(sound) {
+            case 'correct':
+                audioCorrectAnswer.play()
+                break
+            case 'incorrect':
+                audioIncorrectAnswer.play()
+                break
+            case 'finish':
+                audioFinishQuiz.play()
+                break
+        }
+    }
+}
+
+// evento para confirmação de reload dentro do quiz
+window.addEventListener('beforeunload', function (event) {
+    if(!quizSection.classList.contains('hidden')) {
+        event.preventDefault()
+    }
+})
